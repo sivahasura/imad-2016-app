1ca6eedfdfa478cfd97c78dcf560c468beb76f47
sivahasura <siva22india@gmail.com>
2016-09-21 20:56:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,4 +3,4 @@ console.log('Loaded!');
 //change the text of the page that has the madi image
 var element = document.getElementById('main-text');
 
-element.innerHTML = 'New valur';
+element.innerHTML = 'New value';
