1fbcb93905d088e0bdd8a152f7664c387869608b
sivahasura <siva22india@gmail.com>
2016-09-25 13:27:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,21 @@ button.onclick = function () {
 //lets make the request
     request.open(`GET`, `http://sivahasura.imad.hasura-app.io/counter`, true);
     request.send(null);
-
-    
-    
 };
+
+
+//submit name
+var nameInput = document.getElementById (`name`);
+var name = nameInput.value;
+var submit = document.getElementById(`submit.btn`);
+submit.onclick = function () {
+    //this function should make a request to the server and send a name
+    //capture a list of names and render em as a list
+    var names = [`name1`, `name2`, `name3`, 'name4'];
+    var list = '';
+    for (var i = 0; i < names.length; i++) {
+        list += `<li>` + names[i] + `</li>`;
+    }
+   var ul = document.getElementById(`namelist`);
+   ul.innerHTML = list;
+   };
