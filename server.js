9b58181e73725a41b47aa5addda3229166540e44
sivahasura <siva22india@gmail.com>
2016-09-26 21:56:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,117 +1,12 @@
 var express = require('express');
 var morgan = require('morgan');
-var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'article one | siva', 
-    heading: 'article one',
-    date: 'Sep 20th 2016', 
-    content: `<p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
-                </p>
-                <p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
-                </p>
-                <p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
-                </p>
-                <p>
-                    This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. 
-                </p>`
-}
-
-function createTemplate (data) {
-    var title = data.title;
-    var heading = data.heading;
-    var date = data.date;
-    var content = data.content;
-    
-    
-    var htmlTemplate = `<html>
-        <head>
-            <title>
-                ${title}
-            </title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <link href="/ui/style.css" rel="stylesheet" />
-        </head>
-        <body>
-            <div class="container">
-            <div>
-                <a href="/">Home</a>
-            </div>
-            <hr>
-            <h3>
-                  ${heading}
-            </h3>
-            <div>
-                  ${date}
-            </div>
-                <div>
-                  ${content}    
-                </div>
-            </div>
-        </body>
-        </html> 
-        `
-        ;
-        return htmlTemplate;
-        
-}
-
-
-
-
-
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-var counter=0;
-app.get(`/counter`, function(req, res){
-    counter = counter + 1;
-    res.send(counter.toString());
+  res.sendFile('./app/index.html');
 });
 
-app.get(`/article-one`, function(req, res){
-    res.send(createTemplate(articleOne));
-});
-
-app.get(`/article-two`, function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get(`/article-three`, function(req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-var names = [];
-app.get ('/submit-name/:name', function(req, res){
-   //get the name from the request  
-   var name = req.params.name; //1000
-   
-   names.push(name);
-   //JSON : Java Script Object Notation 
-   
-   res.send(JSON.stringify(names));
-});
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(80, function () {
+  console.log('IMAD course app listening on port 80!');
 });
